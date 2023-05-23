16c034c18fe24117faeb4cd3c659ce0992cd43e9
thanhhoax701 <tth1610.vl@gmail.com>
2023-05-23 17:09:00 +0700

    giao diện

--- Code/app/Http/Controllers/HomeController.php
+++ Code/app/Http/Controllers/HomeController.php
@@ -6,9 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         $title = 'home';
-        return view('home', compact('title'));
+        return view('home',compact('title'));
     }
 }
